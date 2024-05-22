b0427ee4c93524b87028d0c038c6ef3a39ca022f
sthurber506 <113561221+sthurber506@users.noreply.github.com>
2024-05-21 22:21:46 -0400

    Update verify_memory_limits.py

--- verify_memory_limits.py
+++ verify_memory_limits.py
@@ -1,10 +1,15 @@
 from dask_cuda import LocalCUDACluster
 from dask.distributed import Client
 
+def get_memory_info():
+    from distributed import get_worker
+    worker = get_worker()
+    return worker.nthreads, worker.memory_limit
+
 def check_memory():
     cluster = LocalCUDACluster()
     client = Client(cluster)
-    result = client.run(lambda: (client.nthreads(), client.memory_limit))
+    result = client.run(get_memory_info)
     print(result)
 
 if __name__ == '__main__':
